6d491f82d8f9c30a925ca15ebd09af62007d19f6
Mihir Sanghrajka <mihir.sangh@gmail.com>
2021-03-23 16:39:15 +0530

    Added and sayHello() function

--- dummy_project/src/dummy_project/SimpleProject.java
+++ dummy_project/src/dummy_project/SimpleProject.java
@@ -1,10 +1,15 @@
 package dummy_project;
 
 public class SimpleProject {
-
+	
+	public  static void sayHello() {
+		System.out.println("Hello");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is a new project");
+		
+		sayHello();
 	}
 
 }
